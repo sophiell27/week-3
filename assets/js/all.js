d4a1e818cac82432d7f69a7b8f38fe003c2b3494
sophiell27 <105938870+sophiell27@users.noreply.github.com>
2022-08-02 22:05:51 +0800

    Update 2022-08-02T14:05:48.783Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,18 @@
 
 console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  // hamburger menu show
+  $(".mobile-nav").click(function (e) {
+    e.preventDefault();
+    $(".header-nav").toggleClass("show-mobile-menu");
+  });
+}); //  hamburger menu hide when click outside
+
+$(document).on("click", function (event) {
+  var $trigger = $(".header-nav");
+
+  if ($trigger !== event.target && !$trigger.has(event.target).length) {
+    $(".header-nav").removeClass("show-mobile-menu");
+  }
 });
 //# sourceMappingURL=all.js.map
