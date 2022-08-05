dfd4856a8ab35706565f52fd76db1782117e7e1c
sophiell27 <105938870+sophiell27@users.noreply.github.com>
2022-08-05 17:25:05 +0800

    Update 2022-08-05T09:25:03.602Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,18 +2,38 @@
 
 console.log('Hello!');
 $(document).ready(function () {
-  // hamburger menu show
-  $(".mobile-nav").click(function (e) {
+  // header: hamburger menu show
+  $(".ham-menu").click(function (e) {
     e.preventDefault();
     $(".header-nav").toggleClass("show-mobile-menu");
+  }); // index page : contact agreement checkbox show 
+
+  $(".agreement-box").click(function () {
+    $(".agreenment-checked").addClass("agreement-checked-active");
   });
-}); //  hamburger menu hide when click outside
+  $(".agreenment-checked").click(function () {
+    $(this).removeClass("agreement-checked-active");
+  }); // header : menu 系列鏡框 drop down
 
-$(document).on("click", function (event) {
-  var $trigger = $(".header-nav");
+  $(".menu > li > a").click(function (e) {
+    e.preventDefault();
+    console.log('H!');
+    $(this).parent().find("ul").toggleClass("show-menu");
+    $(this).parent().siblings().find("ul").removeClass("show-menu");
+  }); //  pagintion 
 
-  if ($trigger !== event.target && !$trigger.has(event.target).length) {
-    $(".header-nav").removeClass("show-mobile-menu");
-  }
-});
+  $(".pagination a").each(function () {
+    if ($(this).attr("href") == location.pathname) {
+      $(this).addClass("page-active");
+    }
+  });
+  $(document).on("click", function (event) {
+    var $trigger = $(".header-nav");
+
+    if ($trigger !== event.target && !$trigger.has(event.target).length) {
+      $(".header-nav").removeClass("show-mobile-menu");
+      $("ul").removeClass("show-menu");
+    }
+  });
+}); //  header: hamburger menu hide when click outside
 //# sourceMappingURL=all.js.map
